1ac151a66cbe98bf5f022246c30c31de17503176
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-02-16 00:22:16 +0000

    Deploy Scm3tt3rLinG/COMP2156 to github.com/Scm3tt3rLinG/COMP2156.git:gh-pages

---
+++ precache-manifest.b8107f882c55d80a3c7fd1c34ed47d24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a35a3563cf3fac87358",
+    "url": "/Scm3tt3rLinG/COMP2156/static/css/main.cd69fab2.chunk.css"
+  },
+  {
+    "revision": "6a35a3563cf3fac87358",
+    "url": "/Scm3tt3rLinG/COMP2156/static/js/main.6a35a356.chunk.js"
+  },
+  {
+    "revision": "c52a3ac050dca6bda28f",
+    "url": "/Scm3tt3rLinG/COMP2156/static/js/runtime~main.c52a3ac0.js"
+  },
+  {
+    "revision": "64d6decb088646398d8f",
+    "url": "/Scm3tt3rLinG/COMP2156/static/js/2.64d6decb.chunk.js"
+  },
+  {
+    "revision": "78e0eed27ead4064a0b0a07e29e9e94c",
+    "url": "/Scm3tt3rLinG/COMP2156/index.html"
+  }
+];
